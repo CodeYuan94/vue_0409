9f21e57720c185547b3c20be2804b67537e84ca0
CodeYuan-94 <540709850@qq.com>
2020-04-10 18:28:32 +0800

    完成了登录功能

--- vue_0409/src/router/index.js
+++ vue_0409/src/router/index.js
@@ -1,27 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+// 拦截路由导航守卫,next()是放行
+router.beforeEach((to, from, next) => {
+  // 登录页放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const token = window.sessionStorage.getItem('token')
+  // 没有token,强制跳转到登录页
+  if (!token) return next('/login')
+  next()
 })
 
 export default router
